7415f758a150ece53f55020890f6d384fc0ed917
SHYAM UNNITHAN <shyam.unnithan@nissanmotor.com>
2022-11-30 07:25:39 +0530

    Updated knex configuration file

--- config/knex.js
+++ config/knex.js
@@ -4,9 +4,9 @@ module.exports = {
     connection: {
       host: '127.0.0.1',
       port: 3306,
-      user: 'conductor',
-      password: 'Mu5iC@lN19ht',
-      database: 'orchestra',
+      user: '<<DBUser>>',
+      password: '<<DBPassword>>',
+      database: '<<DBName>>',
     },
     migrations: {
       directory: '../database/migrations',
